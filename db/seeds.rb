015d6a81763603022a90b850130b893aebfd5edb
Leonardo Vásquez <leonardovavi@unitec.edu>
2018-02-21 18:31:19 -0600

    Added Api

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-l = Location.create(name: "New Yor City")
+l = Location.create(name: "New York City")
 l.recordings.create(temp: 32, status: "cloudy")
 l.recordings.create(temp: 34, status: "rainy")
 l.recordings.create(temp: 30, status: "rainy")
